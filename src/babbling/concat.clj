ec3661be08bcbe65adae0a83ef905665103e6e70
Nicolas Berger <nicoberger@gmail.com>
2013-11-12 20:12:47 -0300

    (let (if -> (if-let

--- src/babbling/concat.clj
+++ src/babbling/concat.clj
@@ -5,10 +5,9 @@
  ([x] x)
  ([x y]
   (lazy-seq
-    (let [s (seq x)]
-      (if s
-        (cons (first s) (concat (rest s) y))
-        y))))
+    (if-let [s (seq x)]
+      (cons (first s) (concat (rest s) y))
+      y)))
   ([x y & others]
    (apply concat (concat x y) others))
 )
